a04d5d61bd29b3662c5d4a1711efa0145dc6b04f
claysmalley <claysmalley@gmail.com>
2013-11-25 14:39:42 -0600

    added error message when install directory doesn't exist

--- install_in_omz.sh
+++ install_in_omz.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+if [ ! -d ~/.oh-my-zsh ]
+	then mkdir ~/.oh-my-zsh
+fi
+
 echo "Creating symlink from here to your oh-my-zsh themes path..."
 
-ln -f powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
-
-echo "
+successmsg="
 Done ! (the symlink is ~/.oh-my-zsh/themes/powerline.zsh-theme)
 
 ----------------------------------------------------------------------------------
@@ -19,3 +21,14 @@ Requirements:
 Vim Powerline patched font: See Powerline for vim for more info.
 Z shell (zsh): See oh-my-zsh for more info.
 Make sure terminal is using 256-colors mode with export TERM=\"xterm-256color\"".
+
+errormsg="
+Uh oh! Couldn't create symlink ~/.oh-my-zsh/themes/powerline.zsh-theme
+Check and see if you have permission to write to that directory."
+
+ln -f powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
+
+if [ $? = 0 ]
+	then echo "$successmsg"
+	else echo "$errormsg"
+fi
